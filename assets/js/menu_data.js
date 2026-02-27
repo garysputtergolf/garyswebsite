94ccc4bbf70ada9acb2bc66c0c149d29158766d8
garysputtergolf <garysputtergolf@gmail.com>
2026-02-27 12:15:57 -0500

    Uploaded Assets

---
+++ assets/js/menu_data.js
@@ -0,0 +1,116 @@
+const MENU_DATA = {
+    "activities": {
+        "title": "Fun & Activities",
+        "sections": [
+            {
+                "title": "Putter Golf",
+                "items": [
+                    { "name": "Adult Golf", "price": "6.00", "description": "Per person" },
+                    { "name": "Kids 3 & Under", "price": "Free", "description": "With paying adult" },
+                    { "name": "Seasonal Pass", "price": "60.00", "description": "Unlimited golf for the season" }
+                ]
+            },
+            {
+                "title": "Outdoor Fun",
+                "items": [
+                    { "name": "Paddle Boats", "price": "6.00", "description": "30 minute ride" },
+                    { "name": "ZipLine", "price": "8.00 - 25.00", "description": "Price varies by number of rides" }
+                ]
+            },
+            {
+                "title": "Gem Mining",
+                "items": [
+                    { "name": "Red Bag", "price": "8.00" },
+                    { "name": "Yellow Bag", "price": "10.00" },
+                    { "name": "B&B Bag", "price": "20.00" },
+                    { "name": "Gem-i-nator", "price": "30.00" },
+                    { "name": "Gemzilla Bucket", "price": "65.00" }
+                ]
+            },
+            {
+                "title": "Rentals",
+                "items": [
+                    { "name": "Gazebo Rental", "price": "15.00", "description": "3 hours, seats ~15" },
+                    { "name": "Pavilion Rental", "price": "60.00", "description": "3 hours, seats ~75" }
+                ]
+            }
+        ]
+    },
+    "food": {
+        "title": "Jiffy Pup Restaurant",
+        "sections": [
+            {
+                "title": "Combo Meals",
+                "note": "Includes fries & soda",
+                "items": [
+                    { "name": "Jiffy Pup Combo", "price": "7.89" },
+                    { "name": "Hamburger Combo", "price": "9.49" },
+                    { "name": "Big Deluxe Combo", "price": "10.99" },
+                    { "name": "Fish Sandwich Combo", "price": "9.99" },
+                    { "name": "Fish N' Chips", "price": "9.49", "description": "No drink included" },
+                    { "name": "Chicken Sandwich Combo", "price": "10.19" },
+                    { "name": "Philly Cheesesteak Combo", "price": "12.79" }
+                ]
+            },
+            {
+                "title": "Sandwiches & More",
+                "items": [
+                    { "name": "Jiffy Pup", "price": "2.99" },
+                    { "name": "All Beef Hot Dog", "price": "3.69" },
+                    { "name": "Texas Hot Dog", "price": "4.25" },
+                    { "name": "Hamburger", "price": "3.99" },
+                    { "name": "Cheeseburger", "price": "4.59" },
+                    { "name": "Deluxe Burger", "price": "5.99" },
+                    { "name": "Chicken Sandwich", "price": "6.69" },
+                    { "name": "Fish Sandwich", "price": "6.49" },
+                    { "name": "Philly Cheesesteak", "price": "8.49" },
+                    { "name": "Grilled Cheese", "price": "3.29" },
+                    { "name": "Chicken Tenders", "price": "6.69" },
+                    { "name": "Wraps w/ Chips", "price": "11.99" }
+                ]
+            },
+            {
+                "title": "Vegetarian",
+                "items": [
+                    { "name": "Veggie Pup", "price": "3.99" },
+                    { "name": "Veggie Burger", "price": "5.99" }
+                ]
+            },
+            {
+                "title": "Sides & Snacks",
+                "items": [
+                    { "name": "French Fries (Sm)", "price": "3.79" },
+                    { "name": "French Fries (Lg)", "price": "4.49" },
+                    { "name": "Curly Fries", "price": "4.99" },
+                    { "name": "Onion Rings", "price": "4.99" },
+                    { "name": "Mozzarella Sticks", "price": "4.99" },
+                    { "name": "Funnel Cake Fries", "price": "3.59" },
+                    { "name": "Nachos & Cheese", "price": "3.99" }
+                ]
+            },
+            {
+                "title": "Salads",
+                "items": [
+                    { "name": "Side Salad", "price": "3.49" },
+                    { "name": "Caesar Salad", "price": "6.39" },
+                    { "name": "Grilled Chicken Salad", "price": "9.99" },
+                    { "name": "Taco Salad", "price": "8.99" }
+                ]
+            },
+            {
+                "title": "Ice Cream Treats",
+                "items": [
+                    { "name": "Soft Serve (Reg)", "price": "2.50" },
+                    { "name": "Soft Serve (Lg)", "price": "3.50" },
+                    { "name": "Hand Dipped (Reg)", "price": "3.50" },
+                    { "name": "Hand Dipped (Lg)", "price": "4.50" },
+                    { "name": "Sundae (Reg)", "price": "5.29" },
+                    { "name": "Sundae (Lg)", "price": "6.29" },
+                    { "name": "Banana Split", "price": "7.99" },
+                    { "name": "Razzles (Reg)", "price": "4.89" },
+                    { "name": "Milkshakes", "price": "Various" }
+                ]
+            }
+        ]
+    }
+};
